d1a4ef8fee22b8e1bf933b4c14a6cf5cd4bd94b2
Shermosa <sararh108@gmail.com>
2021-11-22 20:27:51 +0000

    adding the equations

--- main.m
+++ main.m
@@ -1,9 +1,22 @@
-%First job is to write the question out. Change this 
-var = 1;
-syms x1
+%Write down the z equation and the constrains
 
-z = 2*x1;
+optimising_function = z == 7*x1 + 2*x2 + 3*x3 + x4 + x5 + x6;
+
+constrain1 = 2*x1 + 7*x2 + x3 + x6 == 30; 
+constrain2 =  5*x1 + 8*x2 + 2*x4 <= 70;
+constrain3 = x1 + x2 + x3 >= 20; 
+constrain4 = x1 + x3 + x5 + x6 <= 41;
+%This last constrain is to make sure all values are non-negative
+constrain5 = x1 * x2 * x3 * x4 * x5 * x6 >= 0;
+
+
+
+
+
+%Method just to test Matlab
+disp(method(2));
+
 function[y] = method(x)
-
-    y = z(x);
+    y = 2*x;
 end
+
