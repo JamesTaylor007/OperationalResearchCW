31eb3ec26e25254c706c77cae08743dc77b7badc
(s) James Taylor <cojmt@lunet.lboro.ac.uk>
2021-12-09 16:02:51 +0000

    creating symbolic array

--- graphs.m
+++ graphs.m
@@ -4,17 +4,16 @@ optimising_function = z == 7*x1 + 2*x2 + 3*x3 + x4 + x5 + x6;
 
 %constrains written in canonical form, we don't use them in the code, just
 %for clarity
-constrain1 = 2*x1 + 7*x2 + x3 + x6 + x7 == 30; 
+constrain1 = sym('constrain1_%d',[1 100]);
+for i=1:length(constrain1)
+    constrain1(i) = 2*x1 + 7*x2 + x3 + x6 + x7 == i;
+end
+disp(constrain1)
 constrain2 =  5*x1 + 8*x2 + 2*x4 + x8 == 70;
 constrain3 = x1 + x2 + x6 - x9 + x10 == 20; 
 constrain4 = x1 + x3 + x5 + x6 + x11 == 41;
 %This last constrain is to make sure all values are non-negative
 constrain5 = x1 * x2 * x3 * x4 * x5 * x6 >= 0;
-
-abc = zeros(1);
-abc(1) = constrain1;
-disp(class(constrain1));
-disp(abc)
 
 %We define matrix A with the coefficients of the constrains
 A = [2, 7, 1, 0, 0, 1, 1, 0, 0, 0, 0;
@@ -138,4 +137,6 @@ function[x] = simplexMethodMatrix(matrix, vector, z_coefficients)
       disp("x"+x_values(i)+" = "+xb(i))
   end
 end
+
+
 
